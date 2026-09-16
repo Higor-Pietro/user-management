eaf7e55f17932578d94c9ca23d961569cf0f7d7b
Higor-Pietro <medeiroshigor2018@gmail.com>
2026-09-16 15:30:51 -0300

    feat: adiciona MainCOntroller

---
+++ src/main/java/com/higor/cadastrousuarios/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.higor.cadastrousuarios.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/")
+public class MainController {
+
+    @GetMapping 
+    public String telaInicial(){
+            return ("Bem vindo!");
+    } 
+    
+    
+}
